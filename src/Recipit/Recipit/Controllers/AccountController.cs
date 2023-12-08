7a165872e03f36b541846caf9ef4b6c52a888b26
David Petkov <davidpetkov2006@gmail.com>
2023-12-08 23:42:29 +0200

    Created login/register pages

---
+++ src/Recipit/Recipit/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recipit.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
